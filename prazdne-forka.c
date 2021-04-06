110049ffb3442437f9029d64aa1fa13cdef5e0a9
donino@hpnator <xzatkalik@gmail.com>
2021-04-06 09:03:42 +0200

    prazdny for

---
+++ prazdne-forka.c
@@ -0,0 +1,20 @@
+/* 
+ * Program: Prazdny for cyklus
+ * Dlhy nazov: Frederikov prazdny for cyklus, za ktory dostal zapocet.
+ *
+ * Originalny autor: Fredo
+ * Prepisal: Zatkalusk
+ *
+ * */
+
+int main(){
+
+int i = 0;
+
+for(i=0;i<1000;i++)
+{
+
+}
+
+return 0;
+}
